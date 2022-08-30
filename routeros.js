78a99342ba1b7fbf2414235975336f125dcadf1b
lbsystem <lbsystem@126.com>
2022-08-30 18:26:14 +0800

    vscode

---
+++ routeros.js
@@ -0,0 +1,31 @@
+const RosApi = require('node-routeros').RouterOSAPI;
+const request = require('request');
+
+const res = (url)=>{
+    return new Promise((resolve,reject)=>{
+        request(url,(error,response,body)=>{
+            if(!error && response.statusCode == 200){
+                resolve(body)
+            }
+        })
+    }
+)}
+
+async function run(){
+    console.log(await res("http://www.baidu.com"))
+    const conn = new RosApi({
+        host: '192.168.2.33',
+        user: 'admin',
+        password: 'p34mv160',
+    });
+    
+    conn.connect()
+        .then(() => {
+            conn.write('/ip/address/print').then(data=>{
+                console.log(data)
+            })
+        })
+
+}
+run()
+
