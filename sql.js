45e8e2de116dea710875ff3c02258085cb97aaee
lbsystem <lbsystem@126.com>
2022-08-31 11:07:19 +0800

    webstroe

---
+++ sql.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql")
+const connection = mysql.createConnection({
+    host:"192.168.1.32",
+    user:"root",
+    password:"p34mv160",
+    database:"BGP"
+})
+connection.connect();
+async function _sql(connection,sql){
+    return new Promise((resolve, reject) => {
+        connection.query(sql,(error,res)=>{
+            if(error) console.log(error);
+            resolve(res)
+            connection.end()
+        })
+    })
+
+}
+
+async function run(){
+   let sql="select * from ipip where country='HK'";
+   let res= await _sql(connection,sql);
+    for(i of res){
+        console.log(i.ASN)
+    }
+}
+
+run()
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-// const now = new Date().getTime();
-// for(i=0;i<100000000;i++){
-//     res=i*10
-//     res=res%10
-//     res=res+i
-// }
-// console.log(res)
-// console.log(new Date().getTime()-now)
+const now = new Date().getTime();
+for(i=0;i<100000000;i++){
+    res=i*Math.round(Math.random()*350)
+    res=res%10
+    res=Math.cos(res)
+}
+console.log(res)
+console.log(new Date().getTime()-now)
 
