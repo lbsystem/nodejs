1b956f91896130d7ee16fe7fa64e9aaf2d303761
lbsystem <lbsystem@126.com>
2022-08-31 15:11:06 +0800

    macOS

--- test.js
+++ test.js
@@ -1,25 +1,10 @@
-const async = require('async');
-(async () => {
-    // 支持数组格式
-    let list = [{
-        'url': '1',
-    }, {
-        'url': '2',
-    }];
 
-    //数字10是并发数量
-    async.mapLimit(list, 10, async item => { // <- no callback!
-        let data = await toUrl(item);
-        return data; // <- return a value!
-    }, (err, contents) => {
-        if (err) throw err;
-        console.log(contents);
-    });
-})();
 
-async function toUrl(item) {
 
-    // 。。。 写处理逻辑
-    console.log(item);
-    return '处理完成';
-}
+
+url="okokokok"
+
+
+list1=[1,2,3,5,6]
+let now = new Date().getTime()
+console.log(new Date().getTime()-now)
